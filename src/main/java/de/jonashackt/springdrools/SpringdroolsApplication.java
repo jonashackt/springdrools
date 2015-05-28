78d6be487f0c1207f8115b5e44971f9484e080e3
Jonas Hecht <jonas.hecht@codecentric.de>
2015-05-28 13:51:44 +0200

    initial commit

---
+++ src/main/java/de/jonashackt/springdrools/SpringdroolsApplication.java
@@ -0,0 +1,12 @@
+package de.jonashackt.springdrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdroolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdroolsApplication.class, args);
+    }
+}
